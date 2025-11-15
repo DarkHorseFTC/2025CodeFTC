31c8f586fc661d2cea6255eac53876cc906fa7ce
Savi <savsundaram21@gmail.com>
2025-11-14 17:30:23 -0800

    Update TestDrivebaseTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrivebaseTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrivebaseTeleop.java
@@ -35,7 +35,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-@Disabled
 public class TestDrivebaseTeleop extends LinearOpMode {
 
         // Declare OpMode members for each of the 4 motors.
