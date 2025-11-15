04591955608bb78b3e030293263979738a8b9778
Nick-blip-308 <nickpradhan1212@gmail.com>
2025-11-14 18:27:35 -0800

    Update Testdrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testdrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testdrive.java
@@ -34,7 +34,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-@Disabled
 public class Testdrive extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
